d6e59cfa877c850f4ef86eca74d79d11cac471a6
lucawrabetz <wrabetzluca@gmail.com>
2021-04-14 21:43:20 -0400

    cleanup, renaming, restructure dir

--- INTERDICTION.py
+++ INTERDICTION.py
@@ -1,71 +1,44 @@
-import gurobipy as gp
-from gurobipy import GRB
+import graph
+from gurobipy import *
 
-budget = 1000
-nodes,reward, covered_by = gp.multidict({
-    0: [13, {0}], 
-    1: [7, {0,1,2}], 
-    2: [15, {0,1,2}], 
-    3: [11, {2,3}],
-    4: [12, {2,3,4}], 
-    5: [14, {2,4}], 
-    6: [9, {3,4}], 
-    7: [17, {1,2,4}], 
-    8: [13, {1,4}]
-})
+class MRG_Interdiction:
+    # graph object (includes MRG_model, RGI_Model)
+    G = Graph()
 
-beta = 70
-subsets, coverage, cost = gp.multidict({
-    0 : [{0,1,2}, 32],
-    1 : [{1,2,7,8}, 38],
-    2 : [{1,2,3,4,5,7}, 63],
-    3 : [{3,4,6}, 30],
-    4 : [{4,5,6,7,8}, 49]
-})
+    # Kappa Model
+    KAP_Model = Model("Kappa")
 
-m = gp.Model("Interdictor")
+    def __init__(self, graph_inputfile):
+        '''
+            Set up initial MRG and RGI models.
+        '''
+        self.G.setupMRGmodel()
+        self.G.setupRGImodel()
 
-s = [0,1,2,3,4]
+    def get_cover(self):
+        '''
+            Subroutine to separate a cover using MRG, given the current interdiction policy.
+        '''
+        pass
 
-nodes_covered = set([])
-for k in s:
-    for i in coverage[k]:
-        nodes_covered.add(i)
+    def get_kappa(self):
+        '''
+            Subroutine to compute RHS (kappa) for a given cover separated from MRG.
+        '''
+        pass
 
+    def compress_cover(self):
+        '''
+            Subroutine to turn a non minimal cover into a minimal cover so the constraint can have RHS = 1.
+            Not gonna write this yet, don't worry about it.
+        '''
+        pass
 
-#Variable Generation
+    def solve(self):
+        '''
+            Main algorithm to solve interdiction problem.
 
-z = {}
-for k in s:
-    z[k] = m.addVar(vtype = GRB.BINARY, name = "z_%d"%k)
-
-
-y = {}
-for i in nodes_covered:
-    y[i] = m.addVar(vtype = GRB.BINARY, name = "y_%d"%i)
-
-m.update()
-
-
-#Constraints Generation
-
-for k in s:
-    for i in coverage[k]:
-        m.addConstr(y[i] >= 1 - z[k], name = "interdicted_%d,%d"%(k,i))
-
-m.addConstr(gp.quicksum(y[i]*reward[i] for i in nodes_covered) <= beta, name = "min_reward")
-
-
-m.setObjective(gp.quicksum(z[k] for k in s), GRB.MINIMIZE)
-
-m.update()
-
-m.optimize()
-
-for k in s:
-    if z[k].x > 0.5:
-        print('%s = %d'%(z[k].varName, z[k].x))
-
-for i in nodes_covered:
-    if y[i].x > 0.5:
-        print('%s = %d'%(y[i].varName, y[i].x))
+            Solve RGI by repeatedly solving MRG with current RGI policy (z_bar), and using KAP to find RHS for
+            new constraint added to RGI (v2 - option to make covers minimal instead). Implemented with lazy cuts in gurobi.
+        '''
+        pass
