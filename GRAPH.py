1504c7144fd2d47cc38a3a8951cb03d1616d99a6
lucawrabetz <wrabetzluca@gmail.com>
2021-04-14 22:36:34 -0400

    more cleanup in graph.py - updateMRG function written

--- GRAPH.py
+++ GRAPH.py
@@ -20,10 +20,13 @@ class Graph:
     # adjacency list (nodes)
     AD = []
 
-    #budget
+    # budget
     alpha = 0
 
-    #Subset selection variable
+    # subset interdiction variable
+    z = {}
+
+    # subset selection variable
     x = {}
 
     # node surveillance variable
@@ -34,7 +37,7 @@ class Graph:
 
     def __init__(self, inputfile):
         '''
-            Read graph from input file which must be provided at instantion.
+            Read graph from input file which must be provided at instantiation of class object.
         '''
         f = open(inputfile, 'r')
 
@@ -49,16 +52,21 @@ class Graph:
         #read line 2 fill cost list
         line = f.readline()
         fields = str.split(line)
+
         for i in range(self.m):
             self.c.append(int(fields[i]))
+
         #read line 3 to fill reward list
         line = f.readline()
         fields = str.split(line)
+
         for j in range(self.n):
             self.pi.append(int(fields[j]))
+
         #read rest of lines to fill edges (adjacency)
         for j in range(self.n):
             self.AD.append([])
+
         for line in f:
             fields = line.split(' ')
             i = int(fields[0])
@@ -71,85 +79,76 @@ class Graph:
         '''
             Print graph - can be used and modified for logging purposes.
         '''
-        print('s = %d' % self.m)
-        print('u = %d' % self.n)
-        print('edges = %d' % self.edges)
-        print('b = %d' % self.alpha)
+        print('m = %d' % self.m)
+        print('n = %d' % self.n)
+        print('number of edges = %d' % self.edges)
+        print('budget = %d' % self.alpha)
         for j in range(self.m):
-            print('subset_%d' %j)
+            print('subset/location_%d' %j)
             print(self.AD[j])
-            print('cost = %d' % self.c[j])
+            print('interdiction cost = %d' % self.c[j])
 
         for i in range(self.n):
-            print('node_%d' %i)
-            print('reward = %d' %self.pi[i])
+            print('element/target_%d' %i)
+            print('coverage reward = %d' %self.pi[i])
 
     def setupMRG(self):
         '''
             Set up initial MaxRewardGain (MRG) model.
         '''
-        #z = [1,0,1,0,0] replace "z" with "z"
-
-        x = {}
         for k in range(self.m):
-            x[k] = self.MRG_Model.addVar(vtype=GRB.BINARY, name="x_%d" %k)
-        y = {}
-        for i in range(self.n):
-            y[i] = self.MRG_Model.addVar(vtype=GRB.BINARY, obj= -self.pi[i], name = "y_%d" %i)
-
-        self.MRG_Model.update()
+            self.x[k] = self.MRG_Model.addVar(vtype=GRB.BINARY, name="x_%d" %k)
 
         for i in range(self.n):
-            self.MRG_Model.addConstr((gp.quicksum(x[k] for k in self.AD[i]) >= y[i]), name = "coverage_%d"%i)
-
-        #for k in range(self.m):
-        #    self.MRG_Model.addConstr(x[k] <= 1 - z[k], name = "interdicted_%d"%k)
-
-        self.MRG_Model.addConstr(gp.quicksum(x[k] for k in range(self.m)) <= self.alpha, name = "budget")
+            self.y[i] = self.MRG_Model.addVar(vtype=GRB.BINARY, obj= -self.pi[i], name = "y_%d" %i)
 
         self.MRG_Model.update()
-        self.MRG_Model.setParam
-        self.MRG_Model.optimize()
 
-        #for k in range(self.m):
-        #    print("x_{}: {}".format(k,x[k].x))
+        for i in range(self.n):
+            self.MRG_Model.addConstr((gp.quicksum(self.x[k] for k in self.AD[i]) >= self.y[i]), name = "coverage_%d"%i)
 
-        #for i in range(self.n):
-        #    print("y_{}: {}".format(i,y[i].x))
+        self.MRG_Model.addConstr(gp.quicksum(self.x[k] for k in range(self.m)) <= self.alpha, name = "budget")
         self.MRG_Model.update()
+        # self.MRG_Model.setParam
+        # self.MRG_Model.optimize()
+
         self.MRG_Model.write('MRGModel.lp')
 
     def setupRGI(self):
         '''
             Set up initial RewardGainInterdiction (RGI) model.
         '''
-        subs = [0,1,2,3,4]
-
-        #nodes_covered = set([])
-        #for k in subs:
-        #    for i in coverage[k]:
-        #        nodes_covered.add(i) 
-        z = {}
         for k in range(self.m):
-            z[k] = self.RGI_Model.addVar(vtype = GRB.BINARY, name = "z_%d"%k)
+            self.z[k] = self.RGI_Model.addVar(vtype = GRB.BINARY, name = "z_%d"%k)
 
-
-        y = {}
-        for i in range(self.n):
-            y[i] = self.RGI_Model.addVar(vtype = GRB.BINARY, name = "y_%d"%i)
-        self.RGI_Model.setObjective(gp.quicksum(z[k]*self.c[k] for k in range(self.m)), GRB.MINIMIZE)
-
-
+        self.RGI_Model.setObjective(gp.quicksum(self.z[k]*self.c[k] for k in range(self.m)), GRB.MINIMIZE)
 
         self.RGI_Model.update()
-        self.RGI_Model.optimize()
+        # self.RGI_Model.optimize()
         self.RGI_Model.write('RGIModel.lp')
 
-    def updateMRG(self, z_bar):
-        pass
+    def updateMRG(self):
+        '''
+            Update MRG model to reflect current interdiction policy.
 
-G = Graph('INSTANCE0.txt')
+            This is done by simply looping through every subset and setting an upperbound on MRG.x[k] to 0 if RGI.z[k] is equal to 1. 
+            Otherwise, the upperbound for MRG.x[k] can stay at 1.
+        '''
+        testlist = [1 for i in range(self.m)]
+
+        for k in range(self.m):
+            if testlist[k] > 0.5:
+                # set upper bound on self.x
+                print(self.x[k])
+                self.x[k].ub = 0
+            else:
+                self.x[k].lb =  1
+
+
+G = Graph('dat/instance0.graph')
+G.printG()
 G.setupMRG()
 G.setupRGI()
 
+G.updateMRG()
 
